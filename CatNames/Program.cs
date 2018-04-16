63e5b5207c7df16921d7bee8fb0a841eaaeda7ed
Jijie Chen <jijie.chen@someplace.com>
2018-04-16 19:45:22 +0800

    Added basic project structure

---
+++ CatNames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
